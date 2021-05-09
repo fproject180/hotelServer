33183fb1a24b4d8be8bb62dfbf919856f20b0f14
Ubuntu <ubuntu@ip-172-31-36-161.ap-south-1.compute.internal>
2021-05-09 11:18:48 +0000

    hotelBooking changes

--- routes/hotelRoute.js
+++ routes/hotelRoute.js
@@ -12,25 +12,26 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  var appliances_Status1 = req.body.status1;
-  var appliances_Status2 = req.body.status2;
-  var appliances_Status3 = req.body.status3;
-  var appliances_Status4 = req.body.status4;
-  var appliances_Status5 = req.body.status5;
+  var appliances_Status1 = req.body.appliance_status1;
+  var appliances_Status2 = req.body.appliance_status2;
+  var appliances_Status3 = req.body.appliance_status3;
+  var appliances_Status4 = req.body.appliance_status4;
+  var appliances_Status5 = req.body.appliance_status5;
   connection.query(
-    "UPDATE Hotel_Booking SET appliances_Status1=?,appliances_Status2=?appliances_Status3=?appliances_Status4=?appliances_Status5=?",
-    [appliances_Status1,appliances_Status2,appliances_Status3,appliances_Status4,appliances_Status5],(err, rows, fields) => {
+    "UPDATE Hotel_Booking SET appliance_status1=?,appliance_status2=?,appliance_status3=?,appliance_status4=?,appliance_status5=? WHERE mobileNo=?",
+    [appliances_Status1,appliances_Status2,appliances_Status3,appliances_Status4,appliances_Status5,mobileNo],(err, rows, fields) => {
         if (err) {
             console.log(err);   
         } else {
             console.log("updated");
+		res.sendStatus(200);
         }
     }
   );
 });
 
 router.get("/appliances",(req, res)=>{
-    connection.query("SELECT appliances_Status1,appliances_Status2,appliances_Status3,appliances_Status4,appliances_Status5 FROM Hotel_Booking",(err, rows, fields)=>{
+    connection.query("SELECT appliance_status1,appliance_status2,appliance_status3,appliance_status4,appliance_status5 FROM Hotel_Booking",(err, rows, fields)=>{
         if (err) {
             console.log(err);
         } else {
