1932de2158c1e7208f818babc3acc4e9903cbc1f
platinumpizza29 <kbelgi1@gmail.com>
2021-05-09 15:07:35 +0530

    new hotelBooking

--- routes/hotelRoute.js
+++ routes/hotelRoute.js
@@ -1,14 +1,50 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const connection = require("../services/connection");
 
-router.get('/',(req, res)=>{
-    connection.query('SELECT * FROM Hotel_Booking',(err, rows)=>{
-        if (err) {
-            console.log("No hotels found");
-        } else {
-            res.send(rows);
-        }
-    });
+router.get("/", (req, res) => {
+  var mobileNo = req.body.mobileNo;
+  connection.query(
+    "SELECT hotelName, city, roomType,roomID, roomIP, checkInDate, checkOutDate,  FROM Hotel_Booking WHERE mobileNo=?",
+    [mobileNo],
+    (err, rows) => {
+      if (err) {
+        console.log("No hotels found");
+      } else {
+        res.send(rows);
+      }
+    }
+  );
+});
+
+
+
+
+router.post("/appliances", (req, res) => {
+  var mobileNo = req.body.mobileNo;
+  connection.query(
+    "SELECT appliance1,appliance_status1, appliance2,appliance_status2 appliance3,appliance_status3 appliance4,appliance_status4, appliance5, appliance_status5 FROM Hotel_Booking WHERE mobileNo=?",
+    [mobileNo],
+    (err, rows) => {
+      if (err) {
+        console.log("Error Selecting Appliance");
+      } else {
+        res.send(rows);
+      }
+    }
+  );
+});
+
+router.get("/", (req, res) => {
+  connection.query(
+    "SELECT name, email, password, mobileNo, address,dob FROM Hotel_User",
+    (err, rows) => {
+      if (err) {
+        console.log("No users found!");
+      } else {
+        res.send(rows);
+      }
+    }
+  );
 });
 
 module.exports = router;
--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -13,30 +13,39 @@ router.post("/register", (req, response) => {
   var mobileNo = req.body.mobileNo;
   var address = req.body.address;
   var dob = req.body.dob;
-  connection.query(
-    "INSERT INTO HOTEL_USER VALUES('" +
-      name +
-      "','" +
-      email +
-      "','" +
-      password +
-      "','" +
-      mobileNo +
-      "','" +
-      address +
-      "','" +
-      dob +
-      "')",
-    (err, rows, fields) => {
-      if (err) {
-        console.log("error in creating user");
-        response.sendStatus(400);
-      } else {
-        console.log("user created successfully");
-        response.sendStatus(201);
-      }
+
+  connection.query("SELECT * FROM Hotel_User WHERE mobileNo = ?", [mobileNo],(err, rows, fields) => {
+    if (rows.length > 0) {
+      console.log("User Exist");
+      response.sendStatus(406);
+    } else {
+      connection.query(
+        "INSERT INTO HOTEL_USER VALUES('" +
+          name +
+          "','" +
+          email +
+          "','" +
+          password +
+          "','" +
+          mobileNo +
+          "','" +
+          address +
+          "','" +
+          dob +
+          "')",
+        (err, rows, fields) => {
+          if (err) {
+            console.log("error in creating user");
+            response.sendStatus(400);
+          } else {
+            console.log("user created successfully");
+            response.sendStatus(201);
+          }
+        }
+      );
     }
-  );
+  });
+
 });
 
 module.exports = router;
